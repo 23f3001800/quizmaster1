c17db24ffd461d8778002a9fbf08d2876e91a4c9
Your Name <23f3001800@ds.study.iitm.ac.in>
2025-02-13 20:38:00 +0530

    database created

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template,url_for, redirect
-from flask import current_app as app
-import os
+from flask import Flask
+from models.database import db
+
 
 app=None
 
-from controllers.main import *
 
 def my_app():
     app=Flask(__name__)
-    app.debug=True
+    app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///Quiz_mater.sqlite3"
+    db.init_app(app)
     app.app_context().push()
+    app.debug=True
+
+my_app()
+
+from controllers.main import *
+
 
 if __name__=="__main__":
     app.run(debug=True)
--- controllers/main.py
+++ controllers/main.py
@@ -5,6 +5,11 @@ from flask import current_app as app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/sign_up",methods=["GET","POST"])
+def sign_up():
+    return render_template("signup.html")
+
     
 
 if __name__=="__main__":
